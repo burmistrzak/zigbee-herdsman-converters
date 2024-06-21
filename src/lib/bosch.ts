c10ae25a59a23eea738eec101df4c0afe70417cb
burmistrzak <61958704+burmistrzak@users.noreply.github.com>
2024-06-21 02:04:14 +0200

    Create bosch.ts

---
+++ src/lib/bosch.ts
@@ -0,0 +1,10 @@
+import {Fz, Tz, ModernExtend, Configure} from './types';
+import {presets as e, access as ea} from './exposes';
+import {numeric, NumericArgs, deviceAddCustomCluster, setupConfigureForReporting} from './modernExtend';
+import {Zcl} from 'zigbee-herdsman';
+import {logger} from './logger';
+
+const NS = 'zhc:bosch';
+
+export const boschModernExtend = {
+};
